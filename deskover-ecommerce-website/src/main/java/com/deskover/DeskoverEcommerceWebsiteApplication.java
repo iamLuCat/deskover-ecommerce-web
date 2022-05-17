b7a1c58499d8675bacf548533da3eca7d1c81a89
Huy Nguyen <hnguyenq85@gmail.com>
2022-05-17 21:13:48 +0700

    init project
    Init project for team

---
+++ deskover-ecommerce-website/src/main/java/com/deskover/DeskoverEcommerceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.deskover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskoverEcommerceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskoverEcommerceWebsiteApplication.class, args);
+	}
+
+}
---
+++ deskover-ecommerce-website/src/test/java/com/deskover/DeskoverEcommerceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deskover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskoverEcommerceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
