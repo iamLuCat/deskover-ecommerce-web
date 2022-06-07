7357227c80c5301bb751b12edf0147bd5036a42b
Minh Nguyen <hoaiminh4321@gmail.com>
2022-06-07 14:24:29 +0700

    Fix login not working

--- deskover-dashboard-website/src/app/services/auth.service.ts
+++ deskover-dashboard-website/src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 import {ToastrService} from 'ngx-toastr';
 import {Router} from "@angular/router";
 import {RestApiService} from "@services/rest-api.service";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,14 @@ import {RestApiService} from "@services/rest-api.service";
 export class AuthService {
   public user: any = null;
 
-  constructor(private restApiService: RestApiService, private router: Router, private toastr: ToastrService) {}
+  constructor(private httpClient: HttpClient, private restApiService: RestApiService, private router: Router, private toastr: ToastrService) {
+  }
 
   // Đăng nhập bằng email và mật khẩu, lưu token vào localStorage
   async login({email, password}) {
     try {
-      const data = await this.restApiService.post(`${environment.apiURL}/auth/login`, {email, password}).toPromise();
-      localStorage.setItem('token', data.token);
+      const data = await this.httpClient.post<any>(`${environment.apiURL}/auth/login`, {email, password}).toPromise();
+      localStorage.setItem('token', data.accessToken);
       await this.getProfile();
       this.router.navigate(['/']);
     } catch (e) {
@@ -26,19 +28,24 @@ export class AuthService {
 
   // Đăng ký
   async register({email, password}) {
-    await this.restApiService.post(`${environment.apiURL}/auth/register`, {email, password}).subscribe(
-      (data: any) => {
-        localStorage.setItem('token', data.token);
-        this.getProfile();
-        this.router.navigate(['/']);
-      }
-    );
+    try {
+      const data = await this.httpClient.post<any>(`${environment.apiURL}/auth/register`, {email, password}).toPromise();
+      localStorage.setItem('token', data.accessToken);
+      await this.getProfile();
+      this.router.navigate(['/']);
+    } catch (e) {
+      this.toastr.error(e);
+    }
   }
 
   // Lấy thông tin người dùng
   async getProfile() {
     try {
-      this.user = await this.restApiService.get(`${environment.apiURL}/auth/profile`).toPromise();
+      this.user = await this.httpClient.get<any>(`${environment.apiURL}/auth/profile`, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      }).toPromise();
     } catch (e) {
       this.logout();
       throw e;
--- deskover-dashboard-website/src/app/services/rest-api.service.ts
+++ deskover-dashboard-website/src/app/services/rest-api.service.ts
@@ -12,7 +12,7 @@ export class RestApiService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : ''
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
     })
   };
 
@@ -23,27 +23,22 @@ export class RestApiService {
     return this.httpClient.get(link, this.httpOptions).pipe(catchError(this.handleError));
   }
 
-  // Lấy dữ liệu theo id
   getOne(link: string, id: any): Observable<any> {
     return this.httpClient.get(link + '/' + id, this.httpOptions).pipe(catchError(this.handleError));
   }
 
-  // Tạo mới dữ liệu
   post(link: string, body: any): Observable<any> {
     return this.httpClient.post(link, body, this.httpOptions).pipe(catchError(this.handleError));
   }
 
-  // Cập nhật mới dữ liệu
   put(link: string, id: number, body: any): Observable<any> {
     return this.httpClient.put(link + '/' + id, body, this.httpOptions).pipe(catchError(this.handleError));
   }
 
-  // Xoá dữ liệu
   delete(link: string, id: number) {
     return this.httpClient.delete(link + '/' + id, this.httpOptions).pipe(catchError(this.handleError));
   }
 
-  // Xử lý lỗi
   handleError(error: any) {
     /*let errorMessage = '';
     if (error.error instanceof ErrorEvent) {
