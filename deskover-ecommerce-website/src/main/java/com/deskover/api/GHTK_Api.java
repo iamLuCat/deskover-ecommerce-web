4fa275819fb296d27acb248cd9ef53e0610ef0a5
Minh Nguyen <hoaiminh4321@gmail.com>
2022-06-09 12:30:32 +0700

    Spelling correction, edit method doc comment

--- deskover-ecommerce-website/src/main/java/com/deskover/api/GHTK_Api.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/api/GHTK_Api.java
@@ -18,6 +18,8 @@ import com.deskover.dto.GHTKDto.entity.FeeGhtk;
 import com.deskover.dto.GHTKDto.reponse.AddressReponseData;
 import com.deskover.dto.GHTKDto.reponse.FeeReponseData;
 
+import java.util.Objects;
+
 @RestController
 @RequestMapping("v1/api/ghtk")
 public class GHTK_Api {
@@ -27,9 +29,9 @@ public class GHTK_Api {
 	
 	
 	/**
-	 * @param fee
-	 * @return
-	 * @throws Exception
+	 * @param fee the fee of GHTK
+	 * @return the fee
+	 * @throws Exception if an error occurs
 	 */
 	@PostMapping(path = "/fee", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> Fee(@RequestBody FeeGhtk fee) throws Exception {
@@ -43,7 +45,7 @@ public class GHTK_Api {
 		try {
 			HttpEntity<FeeGhtk> request = new HttpEntity<>(fee, headers);
 			FeeReponseData response = restTemplate.postForObject(url, request, FeeReponseData.class);
-			if(response.getFee() == null) {
+			if(Objects.requireNonNull(response).getFee() == null) {
 				ResponseEntity<String> errorRequest = restTemplate.postForEntity(url, request, String.class);
 				return ResponseEntity.ok(errorRequest.getBody());
 			}
@@ -55,12 +57,12 @@ public class GHTK_Api {
 	}
 	
 	/**
-	 * @param addressReponse
-	 * @return
-	 * @throws Exception
+	 * @param addressResponse the address of GHTK
+	 * @return the address
+	 * @throws Exception if an error occurs
 	 */
 	@GetMapping(path = "/shipment/list_address", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<?> doGetListAddress(@RequestBody AddressReponseData  addressReponse) throws Exception {
+	public ResponseEntity<?> doGetListAddress(@RequestBody AddressReponseData  addressResponse) throws Exception {
 
 		String url = UrlConstant.GHTK_LIST_ADDRESS; 
 
@@ -71,7 +73,7 @@ public class GHTK_Api {
 		
 		System.out.println(headers);
 		
-		HttpEntity<AddressReponseData> request = new HttpEntity<>(addressReponse, headers);
+		HttpEntity<AddressReponseData> request = new HttpEntity<>(addressResponse, headers);
 		
 		AddressReponseData response = restTemplate.postForObject(url, request, AddressReponseData.class);
 
