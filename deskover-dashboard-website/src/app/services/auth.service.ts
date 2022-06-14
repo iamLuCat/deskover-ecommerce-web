5a6a5dda5125a660aa8dab9dd666cf3eac61042e
Minh Nguyen <hoaiminh4321@gmail.com>
2022-06-14 16:52:33 +0700

    Update dashboard service

--- deskover-dashboard-website/src/app/services/auth.service.ts
+++ deskover-dashboard-website/src/app/services/auth.service.ts
@@ -4,6 +4,8 @@ import {ToastrService} from 'ngx-toastr';
 import {Router} from "@angular/router";
 import {RestApiService} from "@services/rest-api.service";
 import {HttpClient} from "@angular/common/http";
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +19,7 @@ export class AuthService {
   // Đăng nhập bằng email và mật khẩu, lưu token vào localStorage
   async login({email, password}) {
     try {
-      const data = await this.httpClient.post<any>(`${environment.apiURL}/auth/login`, {email, password}).toPromise();
+      const data = await this.httpClient.post<any>(`${environment.apiURL}/auth/login`, {email, password}).pipe(catchError(this.handleError)).toPromise();
       localStorage.setItem('token', data.accessToken);
       await this.getProfile();
       this.router.navigate(['/']);
@@ -29,7 +31,7 @@ export class AuthService {
   // Đăng ký
   async register({email, password}) {
     try {
-      const data = await this.httpClient.post<any>(`${environment.apiURL}/auth/register`, {email, password}).toPromise();
+      const data = await this.httpClient.post<any>(`${environment.apiURL}/auth/register`, {email, password}).pipe(catchError(this.handleError)).toPromise();
       localStorage.setItem('token', data.accessToken);
       await this.getProfile();
       this.router.navigate(['/']);
@@ -45,7 +47,7 @@ export class AuthService {
         headers: {
           'Authorization': `Bearer ${localStorage.getItem('token')}`
         }
-      }).toPromise();
+      }).pipe(catchError(this.handleError)).toPromise();
     } catch (e) {
       this.logout();
       throw e;
@@ -57,5 +59,15 @@ export class AuthService {
     localStorage.removeItem('token');
     this.user = null;
     this.router.navigate(['/login']);
+  }
+
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\n - Message: ${error.message}`;
+    }
+    return throwError(errorMessage);
   }
 }
