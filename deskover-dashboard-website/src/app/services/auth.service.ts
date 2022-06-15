1921a7e3d598e34f853ec36f6e7077da588080c1
Minh Nguyen <hoaiminh4321@gmail.com>
2022-06-16 03:47:18 +0700

    Generate and config interceptor

--- deskover-dashboard-website/src/app/services/auth.service.ts
+++ deskover-dashboard-website/src/app/services/auth.service.ts
@@ -63,11 +63,13 @@ export class AuthService {
 
   handleError(error: any) {
     let errorMessage = '';
+
     if (error.error instanceof ErrorEvent) {
       errorMessage = error.error.message;
     } else {
       errorMessage = `Error Code: ${error.status}\n - Message: ${error.message}`;
     }
+
     return throwError(errorMessage);
   }
 }
