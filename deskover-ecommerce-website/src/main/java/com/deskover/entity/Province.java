d632e56fc57422d5f44fde6cb9d41399f4c7af21
Hải Phạm <haipham2001vn@gmail.com>
2022-07-02 09:14:34 +0700

    update database and entity

---
+++ deskover-ecommerce-website/src/main/java/com/deskover/entity/Province.java
@@ -0,0 +1,32 @@
+package com.deskover.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "province")
+public class Province implements Serializable {
+    private static final long serialVersionUID = 7746926219190939730L;
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "_name", length = 50)
+    private String name;
+
+    @Column(name = "_code", length = 20)
+    private String code;
+
+}
