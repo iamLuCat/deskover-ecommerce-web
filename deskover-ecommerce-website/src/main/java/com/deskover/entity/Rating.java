26c75e2930d46fe10f5b8beee5c335f3cabe05a3
Hải Phạm <haipham2001vn@gmail.com>
2022-07-05 20:58:24 +0700

    update entity

---
+++ deskover-ecommerce-website/src/main/java/com/deskover/entity/Rating.java
@@ -0,0 +1,50 @@
+package com.deskover.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "rating")
+public class Rating implements Serializable {
+    private static final long serialVersionUID = -8794052286728363397L;
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "fullname", nullable = false)
+    private String fullname;
+
+    @Column(name = "phone", length = 10)
+    private String phone;
+
+    @Column(name = "point", nullable = false)
+    private Integer point;
+
+    @Lob
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "actived", nullable = false)
+    private Boolean actived = false;
+
+    @Column(name = "modified_at", nullable = false)
+    @CreationTimestamp
+    private Timestamp modifiedAt;
+
+}
