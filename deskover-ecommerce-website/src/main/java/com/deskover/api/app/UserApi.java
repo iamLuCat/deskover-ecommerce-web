70cd191f7aa60cc4483a1ff7bb3fbac6c8127baf
Minh Nguyen <hoaiminh4321@gmail.com>
2022-07-24 07:10:09 +0700

    Remove unused imports

--- deskover-ecommerce-website/src/main/java/com/deskover/api/app/UserApi.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/api/app/UserApi.java
@@ -1,18 +1,11 @@
 package com.deskover.api.app;
 
-import com.deskover.dto.app.order.resquest.AddOrderResponse;
-import com.deskover.entity.Order;
-import com.deskover.entity.OrderDetail;
-import com.deskover.entity.OrderItem;
 import com.deskover.service.UserAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController("UserApiForClient")
 @RequestMapping("v1/api/custumer")
