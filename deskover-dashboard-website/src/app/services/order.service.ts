f228d99f3766ef81791aeed447a82d36eb1c9e43
Minh Nguyen <hoaiminh4321@gmail.com>
2022-07-24 14:24:58 +0700

    ---

---
+++ deskover-dashboard-website/src/app/services/order.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {RestApiService} from "@services/rest-api.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  url = environment.globalUrl.orderApi;
+
+  constructor(private restApi: RestApiService) { }
+
+  getOrdersForDatatables(params: any) {
+    return this.restApi.getWithParams(this.url + '/datatables', params);
+  }
+}
