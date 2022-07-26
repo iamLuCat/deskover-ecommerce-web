c8a97652c80ab4ed475f4fa5aee58394e0b96b90
Pham Quang Vu <87456448+QuangVu1235@users.noreply.github.com>
2022-07-26 09:10:59 +0700

    update entity ordershiping

--- deskover-ecommerce-website/src/main/java/com/deskover/dto/ghtk/entity/OrderShipping.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/dto/ghtk/entity/OrderShipping.java
@@ -4,6 +4,8 @@ public class OrderShipping {
 	String partner_id; // orderID
 	String label; // Mã vận đơn
 	Double fee; // phí ship
+	String estimated_pick_time;//dự kiến lấy hàng
+	String estimated_deliver_time;//dự kiến giao hàng
 	
 
 }
