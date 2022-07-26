0a6a93046224185e269e6586b27e80fb51eff1b0
Hải Phạm <haipham2001vn@gmail.com>
2022-07-26 15:34:33 +0700

    update Address Api

--- deskover-ecommerce-website/src/main/java/com/deskover/repository/DistrictReponsitory.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/repository/DistrictReponsitory.java
@@ -1,7 +1,11 @@
 package com.deskover.repository;
 
-import com.deskover.entity.District;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.deskover.entity.District;
+
 public interface DistrictReponsitory extends JpaRepository<District, Long> {
+	List<District> findByProvinceId(Long provinceId);
 }
---
+++ deskover-ecommerce-website/src/main/java/com/deskover/service/ProvinceService.java
@@ -0,0 +1,9 @@
+package com.deskover.service;
+
+import java.util.List;
+
+import com.deskover.entity.Province;
+
+public interface ProvinceService {
+	List<Province> getAll();
+}
