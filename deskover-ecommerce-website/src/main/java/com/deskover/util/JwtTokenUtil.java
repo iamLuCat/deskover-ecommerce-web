4189f87cbed40327a78ab438fe7cf7d438070f62
Minh Nguyen <hoaiminh4321@gmail.com>
2022-07-28 07:43:12 +0700

    Update JwtTokenUtil.java

--- deskover-ecommerce-website/src/main/java/com/deskover/util/JwtTokenUtil.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/util/JwtTokenUtil.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 
 @Component
 public class JwtTokenUtil implements Serializable {
-	public static final long JWT_TOKEN_VALIDITY = 5;
+	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 	private static final long serialVersionUID = 19984451291517782L;
 
 	@Value("${jwt.secret}")
