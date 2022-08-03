70bf85c26a4b6ec1dc909ebf9bf5555ec1ae62d0
hnguyen416 <huyquang.nguyen@dxc.com>
2022-08-03 22:26:15 +0700

    Update front end

--- deskover-ecommerce-website/src/main/java/com/deskover/service/impl/UserServiceImpl.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/service/impl/UserServiceImpl.java
@@ -95,4 +95,6 @@ public class UserServiceImpl implements UserService {
 		userPasswordService.updatePassword(SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication().getName(), userRequest);
 	}
 
+
+
 }
