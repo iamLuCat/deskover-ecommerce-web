8c54a30b7a09b22fa0567aa35f622338c7982113
BuiDucMinhx <71239318+BuiDucMinhx@users.noreply.github.com>
2022-08-10 08:32:05 +0700

    update invoice

--- deskover-ecommerce-website/src/main/java/com/deskover/controller/rest/api/TestApi.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/controller/rest/api/TestApi.java
@@ -3,9 +3,18 @@ package com.deskover.controller.rest.api;
 import com.deskover.model.entity.dto.security.payload.MessageResponse;
 import com.deskover.model.entity.other.MailInfo;
 import com.deskover.other.util.MailUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.IOException;
 
 import javax.mail.MessagingException;
 
@@ -13,11 +22,11 @@ import javax.mail.MessagingException;
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/test")
 public class TestApi {
-    @Autowired
+	@Autowired
     private MailUtil mailUtil;
 
     @GetMapping("/email")
-    public ResponseEntity<?> test(@RequestParam String to) {
+    public ResponseEntity<?> test(@RequestParam String to) throws IllegalStateException, IOException {
         System.out.println(to);
         try {
             MailInfo mailInfo = new MailInfo();
@@ -25,6 +34,7 @@ public class TestApi {
             mailInfo.setTo(to);
             mailInfo.setSubject("Test");
             mailInfo.setBody("Hello World");
+            
             mailUtil.push(mailInfo);
             return ResponseEntity.ok(new MessageResponse("Send email successfully"));
         } catch (MessagingException e) {
