4b827939679976ec350edce3c7f5eaeb51327af4
Minh Nguyen <hoaiminh4321@gmail.com>
2022-08-11 13:51:17 +0700

    Update report service

--- deskover-ecommerce-website/src/main/java/com/deskover/service/StatisticService.java
+++ deskover-ecommerce-website/src/main/java/com/deskover/service/StatisticService.java
@@ -14,5 +14,5 @@ public interface StatisticService {
 
 	List<OrderReport> getQuantityProductSoldBySubcategory();
 
-	List<ProductReport> getTop5ProductSold();
+	List<ProductReport> getTopProductSold(Integer limit);
 }
